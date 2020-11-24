c95e1481d82f59821723894686949190ff6d54bc
Tim Morgan <git@timothymorgan.info>
2020-11-24 12:08:13 -0800

    Fix fallback issue

--- src/i18n/messages.ts
+++ src/i18n/messages.ts
@@ -1,5 +1,6 @@
 import { LocaleMessages } from 'vue-i18n'
 import en from '@/i18n/strings/en'
 
-const messages: LocaleMessages = { en }
+const messages: LocaleMessages = { en, 'en-us': en }
+// necessary because $te does not check fallbacks
 export default messages
