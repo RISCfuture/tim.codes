bb9ec11b928c7cd2207bcc46252d6dade0ff6172
Tim Morgan <github@timothymorgan.info>
2025-10-31 01:47:27 -0700

    Package updates

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,3 +1,29 @@
+// Mock localStorage for tests
+const localStorageMock = {
+  getItem: function (key: string): string | null {
+    return this[key] || null
+  },
+  setItem: function (key: string, value: string) {
+    this[key] = value
+  },
+  removeItem: function (key: string) {
+    delete this[key]
+  },
+  clear: function () {
+    const keys = Object.keys(this)
+    keys.forEach((key) => {
+      if (typeof this[key] !== 'function') {
+        delete this[key]
+      }
+    })
+  }
+}
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+  writable: true
+})
+
 // Mock window.matchMedia for tests
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
